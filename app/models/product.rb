00d71aa044116d124091d33e74f7e28e71007446
Renee Nilsen <reneen3833@gmail.com>
2019-11-06 13:44:49 +1000

    validates product params

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,13 @@ belongs_to :user
 has_one_attached :image
 has_many :cart_products, dependent: :destroy
 has_many :carts, through: :cart_products
-# before_commit :add_default_img
+before_commit :add_default_img
 
-validates :title, :description, :category, presence: true
-validates :quantity, :price, presence: true, numericality: true
+validates :category, presence: true
+validates :title, presence: true, length: { minimum: 5 }
+validates :description, presence: true, length: { minimum: 200 }
+validates :quantity, presence: true, :numericality => { :greater_than_or_equal_to => 1 }
+validates :price, presence: true, :numericality => { :greater_than => 0 }
 
   # check if current user is the owner of the product or the admin
   def can_edit?(user)
@@ -16,7 +19,7 @@ validates :quantity, :price, presence: true, numericality: true
 
   def add_default_img
     unless image.attached? 
-    self.image.attach(io: File.open(Rails.root.join("app", "assets", "images", "logo.png")), filename: 'logo.png', content_type: "image/png")
+    self.image.attach(io: File.open(Rails.root.join("app", "assets", "images", "no-image-770x466.jpg")), filename: 'logo.png', content_type: "image/jpg")
     end
   end
 
